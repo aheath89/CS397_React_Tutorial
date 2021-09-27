7e49a05d61a781b4dfa2131ab22b1021ef81216b
Abby Heath <abbyheath@Abbys-MacBook-Pro-2.local>
2021-09-26 19:33:41 -0500

    read data

---
+++ src/utilities/firebase.js
@@ -0,0 +1,39 @@
+import { useState, useEffect } from 'react';
+import { initializeApp } from 'firebase/app';
+import { getDatabase, onValue, ref, set } from 'firebase/database';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDvC7lYPki-ySIp5D4xvQ3c3GsuRMIRPeA",
+    authDomain: "cs397-react-tutorial-274c9.firebaseapp.com",
+    databaseURL: "https://cs397-react-tutorial-274c9-default-rtdb.firebaseio.com",
+    projectId: "cs397-react-tutorial-274c9",
+    storageBucket: "cs397-react-tutorial-274c9.appspot.com",
+    messagingSenderId: "847122675704",
+    appId: "1:847122675704:web:b0317af283dd5f5552080d"
+};
+
+const firebase = initializeApp(firebaseConfig);
+const database = getDatabase(firebase);
+
+export const useData = (path, transform) => {
+    const [data, setData] = useState();
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState();
+  
+    useEffect(() => {
+        const dbRef = ref(database, path);
+        return onValue(dbRef, (snapshot) => {
+            const val = snapshot.val();
+            setData(transform ? transform(val) : val);
+            setLoading(false);
+            setError(null);
+        }, (error) => {
+            setData(null);
+            setLoading(false);
+            setError(error);
+        });
+    }, [path, transform]);
+  
+    return [data, loading, error];
+};
+
