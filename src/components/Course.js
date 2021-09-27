c2a78140a49a515158fb38076211d8c96c5cd8fb
Abby Heath <abbyheath@Abbys-MacBook-Pro-2.local>
2021-09-26 20:31:03 -0500

    auth

--- src/components/Course.js
+++ src/components/Course.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { hasConflict, getCourseTerm, timeParts } from '../utilities/times';
-import { setData, useData } from '../utilities/firebase';
+import { setData, useUserState } from '../utilities/firebase';
 
 const getCourseNumber = course => (
     course.id.slice(1, 4)
@@ -18,7 +18,7 @@ const getCourseMeetingData = course => {
     return null;
 };
 
-const reschedule = async (course, meets, selected, setSelected) => {
+const reschedule = async (course, meets) => {
     if (meets && window.confirm(`Change ${course.id} to ${meets}?`)) {
       try {
         await setData(`courses/${course.id}/meets`, meets);
@@ -31,6 +31,7 @@ const reschedule = async (course, meets, selected, setSelected) => {
 const Course = ({ course, selected, setSelected }) => {
     const isSelected = selected.includes(course);
     const isDisabled = !isSelected && hasConflict(course, selected);
+    const [user] = useUserState();
     const style = {
       backgroundColor: isDisabled? 'lightgrey' : isSelected ? 'lightgreen' : 'white'
     };
@@ -39,7 +40,7 @@ const Course = ({ course, selected, setSelected }) => {
       <div className="card m-1 p-2" 
           style={style}
           onClick={isDisabled ? null : () =>  setSelected(toggle(course, selected))}
-          onDoubleClick={() => reschedule(course, getCourseMeetingData(course), selected, setSelected)}>
+          onDoubleClick={!user ? null : () => reschedule(course, getCourseMeetingData(course))}>
         <div className="card-body">
           <div className="card-title">{ getCourseTerm(course) } CS { getCourseNumber(course) }</div>
           <div className="card-text">{ course.title }</div>
--- src/utilities/firebase.js
+++ src/utilities/firebase.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import { initializeApp } from 'firebase/app';
 import { getDatabase, onValue, ref, set } from 'firebase/database';
+import { getAuth, GoogleAuthProvider, onIdTokenChanged, signInWithPopup, signOut } from 'firebase/auth';
 
 const firebaseConfig = {
     apiKey: "AIzaSyDvC7lYPki-ySIp5D4xvQ3c3GsuRMIRPeA",
@@ -40,3 +41,21 @@ export const useData = (path, transform) => {
 export const setData = (path, value) => (
     set(ref(database, path), value)
 );
+
+export const signInWithGoogle = () => {
+    signInWithPopup(getAuth(firebase), new GoogleAuthProvider());
+};
+
+const firebaseSignOut = () => signOut(getAuth(firebase));
+
+export { firebaseSignOut as signOut };
+
+export const useUserState = () => {
+    const [user, setUser] = useState();
+  
+    useEffect(() => {
+      onIdTokenChanged(getAuth(firebase), setUser);
+    }, []);
+  
+    return [user];
+};
